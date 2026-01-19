2ae4e87546edc3186015e73e15f3a019a3e86854
ulageshwaran <ulageshofficial@gmail.com>
2026-01-19 17:09:31 +0530

    Settings updated

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage', 
     'django.contrib.staticfiles',
     'cloudinary',
     'portfolio',
